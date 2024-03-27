a8a3554b464aa8a422f536d09faa8eb91485e8bd
Xuebing Li <johnsonli1993@gmail.com>
2024-03-27 07:53:41 +0000

    update state_inference.ino

--- state_inference/state_inference.ino
+++ state_inference/state_inference.ino
@@ -5,15 +5,8 @@
 
 // SCL  D2  00000100  0x04
 // SDA  D3  00001000  0x08
-// SCL+SDA  00001100  0x0c
-
-#define SCL_MASK 0x04
-#define SDA_MASK 0x08
-#define I2C_MASK 0x0c
-
-#define SCL (byte) (PIND & SCL_MASK)
-#define SDA (byte) ((PIND >> 3) & 0x01)
-#define I2C (byte) (PIND & I2C_MASK)
+#define SCL_PIN 2
+#define SDA_PIN 3
 
 // the sample buffer
 char *buffer;
@@ -22,68 +15,13 @@ unsigned long start_time;
 void setup() {
   start_time = millis();
   Serial.begin(115200);
-  Serial.println("Detecting I2C clock signal...");
-  buffer = (char *) malloc(2048);
-  if (buffer == NULL) {
-    Serial.println("[ERR] Failed to allocate memory for buffer");
-    return;
-  }
+  Serial.println("Monitoring I2C communication...");
+  // buffer = (char *) malloc(2048);
+  // if (buffer == NULL) {
+  //   Serial.println("[ERR] Failed to allocate memory for buffer");
+  //   return;
+  // }
 }
-
-void log(String &msg) {
-  unsigned long elapsed = millis() - start_time;
-  Serial.print(elapsed);
-  Serial.print(" - ");
-  Serial.println(msg);
-}
-
-// void loop0() {
-//   // The log [H, L] means that the SCL is high and the SDA is low
-
-//   // Step 0: both SCL and SDA are high
-//   log("Waiting for the start condition [H, H]");
-//   while (I2C != I2C_MASK) {
-//     // Wait for the initial condition
-//   }
-
-//   // Step 1: SDA becomes low
-//   log("Waiting for [H, L]");
-//   while (I2C != SCL_MASK) {
-//     // Wait for SDA to become low
-//   }
-
-//   // Step 2: SCL becomes low
-//   log("Waiting for [L, L]");
-//   while (I2C != 0) {
-//     // Wait for SCL to become low
-//     if (I2C == SDA_MASK) {
-//       log("SDA is high, aborting");
-//       return;
-//     }
-//   }
-
-//   // Step 3: SCL pulse
-//   while (true) {
-//     while (!SCL) {
-//       // Wait for [H, X]
-//     }
-//     // [H, X], it is time to read data
-//     char data = 0;
-//     bool first = true;
-//     while (SCL) {
-//       if (!first) {
-//         if (!data && SDA) {
-//           // [H, H], STOP condition
-//           log("Termination condition reached");
-//           return;
-//         }
-//       }
-//       data = SDA;
-//       first = false;
-//     }
-//     // [L, X], data is ready
-//   }
-// }
 
 void loop() {
   // Background information: Data Line (SDA) Changes
@@ -102,33 +40,41 @@ void loop() {
   //   2.1 If data changes from 1 to 0, it is a START condition
   //   2.2 If data changes from 0 to 1, it is a STOP condition
 
+  unsigned char data = 0;
+  bool first_in_pulse = true;
+  char condition = 0; // 0: normal data, 1: START, 2: STOP
+
   // Read the signals one by one. 
   // Each signal is either a bit or a start/stop condition
   while (true) {
-    // Wait for the SCL to be high
-    while (!SCL) { }
+    // Read SCL and SDA
+    unsigned char signal = PIND;
+    unsigned char scl = (signal >> SCL_PIN) & 0x01;
+    unsigned char sda = (signal >> SDA_PIN) & 0x01;
 
-    // SCL is high, read the data
-    unsigned char data = 0;
-    bool first = true;
-    char condition = 0; // 0: normal data, 1: START, 2: STOP
-    while (SCL) {
-      if (!first) {
-        if (data != SDA) {
-          if (condition > 0) {
-            // SDA should not change twice during a high SCL
-            Serial.println("ERROR: SDA changes twice during a high SCL");
-          }
-          if (data) {
-            condition = 1;
-          } else {
-            condition = 2;
-          }
+    // Do not process SDA if the SCL is low
+    if (!scl) {
+      first_in_pulse = true;
+      continue;
+    }
+
+    // SCL is high, process SDA
+    if (!first_in_pulse) {
+      if (data != sda) {
+        if (condition > 0) {
+          // SDA should not change twice during a high SCL
+          Serial.println("ERROR: SDA changes twice during a high SCL");
+        }
+        if (data) {
+          condition = 1;
+        } else {
+          condition = 2;
         }
       }
-      data = SDA;
-      first = false;
     }
+    data = sda;
+    first_in_pulse = false;
+  
     unsigned long elapsed = millis() - start_time;
     Serial.print(elapsed);
     Serial.print(" - ");
